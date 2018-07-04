3ed54b41889a90085ac5c9abffe5f245966fa6a1
Maxim Fischuk <mfischuk@vareger.com>
2018-07-04 14:57:18 +0300

    Added user-friendly truffle configuration

--- truffle.js
+++ truffle.js
@@ -1,47 +1,47 @@
 "use strict";
-
+const ApiKey = require('./network_keys/api/infura');
+const Infura = {
+    Mainnet: "https://mainnet.infura.io/" + ApiKey,
+    Ropsten: "https://ropsten.infura.io/" + ApiKey,
+    Rinkeby: "https://rinkeby.infura.io/" + ApiKey,
+    Kovan: "https://kovan.infura.io/" + ApiKey
+}
 const Web3 = require("web3");
-const ProviderEngine = require("web3-provider-engine");
-const WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-const Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
-const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
-
-const ethereumjsWallet = require('ethereumjs-wallet');
-
-// Create wallet from existing private key
-const privateKey = require('./network_keys/private/rinkeby');
-const wallet = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
-const sender = "0x" + wallet.getAddress().toString("hex");
-
-// Using rinkeby testnet
-const apiKey = require('./network_keys/api/infura');
-const providerUrl = "https://rinkeby.infura.io/" + apiKey;
-const engine = new ProviderEngine();
-
-const provider = new Web3.providers.HttpProvider(providerUrl);
-const web3 = new Web3(provider);
-
-// filters
-engine.addProvider(new FilterSubprovider());
-engine.addProvider(new WalletSubprovider(wallet, {}));
-engine.addProvider(new Web3Subprovider(provider));
-engine.start();  // FIXME: Truffle hangs after compilation/migration because of this
-
+const Provider = require('truffle-privatekey-provider');
+const Wallets = require('./network_keys/private/wallets');
+const web3 = new Web3(null);
 
 module.exports = {
     networks: {
-        development: {
-            host: "localhost",
+        test: {
+            host: "127.0.0.1",
             port: 8545,
-            network_id: "*", // Match any network id
-            gas: 4000000,
+            network_id: 5777, // Match Ganache(Truffle) network id
+            gas: 5000000,
         },
         rinkeby: {
             network_id: 4,
-            provider: () => engine,
-            from: sender,
+            provider: () => new Provider(Wallets.Rinkeby, Infura.Rinkeby),
             gas: 4712388,
             gasPrice: web3.toWei(20, 'gwei')
+        },
+        mainnet: {
+            network_id: 1,
+            provider: () => new Provider(Wallets.Mainnet, Infura.Mainnet),
+            gas: 5000000,
+            gasPrice: web3.toWei(12, 'gwei')
+        },
+        ropsten: {
+            network_id: 1,
+            provider: () => new Provider(Wallets.Ropsten, Infura.Ropsten),
+            gas: 5000000,
+            gasPrice: web3.toWei(4, 'gwei')
+        },
+        kovan: {
+            network_id: 1,
+            provider: () => new Provider(Wallets.Kovan, Infura.Kovan),
+            gas: 5000000,
+            gasPrice: web3.toWei(4, 'gwei')
         }
     },
     solc: {
