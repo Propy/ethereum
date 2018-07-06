b1255822fc825f548520300b329402e4fc7c0906
Maxim Fischuk <mfischuk@vareger.com>
2018-07-06 13:50:41 +0300

    Added script for run full test

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+GANACHE=0
+./node_modules/.bin/ganache-cli -i 5777 >> ganache_cli.log 2>&1 & > ${GANACHE}
+sleep 5
+
+npm build
+npm run migrate-test
+npm test
+
+kill -9 ${GANACHE}
