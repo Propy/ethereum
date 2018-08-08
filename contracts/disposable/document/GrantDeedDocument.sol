7c9fb118c4e5ebf08f991d7e2b576ff2a06ccf20
Maxim Fischuk <mfischuk@vareger.com>
2018-08-08 11:09:27 +0300

    Added additional legal information for the document

--- contracts/disposable/document/GrantDeedDocument.sol
+++ contracts/disposable/document/GrantDeedDocument.sol
@@ -5,8 +5,12 @@ import "./BaseDocument.sol";
 contract GrantDeedDocument is BaseDocument {
 
     address public deed;
+    address public agent;
+    uint256 public deedDate;
+    bytes32 public name;
+    bytes32 public referenceNumber;
 
-    constructor(bytes32 _hash, address _deed) BaseDocument(_hash) {
+    constructor(bytes32 _hash, address _deed) public BaseDocument(_hash) {
         deed = _deed;
     }
 
@@ -14,8 +18,38 @@ contract GrantDeedDocument is BaseDocument {
         return keccak256("grantdeed");
     }
 
+    function setData(
+        address _deed,
+        address _agent,
+        uint256 _date,
+        bytes32 _name,
+        bytes32 _ref
+    ) public isNotFinalized onlyContractOwner {
+        deed = _deed;
+        agent = _agent;
+        deedDate = _date;
+        name = _name;
+        referenceNumber = _ref;
+    }
+
     function setDeed(address _deed) public isNotFinalized onlyContractOwner {
         deed = _deed;
+    }
+
+    function setAgent(address _agent) public isNotFinalized onlyContractOwner {
+        agent = _agent;
+    }
+
+    function setDeedDate(uint256 _date) public isNotFinalized onlyContractOwner {
+        deedDate = _date;
+    }
+
+    function setName(bytes32 _name) public isNotFinalized onlyContractOwner {
+        name = _name;
+    }
+
+    function setReferenceNumber(bytes32 _ref) public isNotFinalized onlyContractOwner {
+        referenceNumber = _ref;
     }
 
 }
