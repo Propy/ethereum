d942a39d27425fbfbf382f0ea0b0bc399cb78807
Maxim Fischuk <mfischuk@vareger.com>
2018-08-23 09:53:20 +0300

    Updated Escrow events

--- contracts/disposable/configurable/Escrow.sol
+++ contracts/disposable/configurable/Escrow.sol
@@ -25,13 +25,14 @@ contract Escrow is Owned, AddressChecker {
 
     /// EVENTS ///
 
-    event PaymentReceived(address indexed who, uint256 value);                          // Native blockchain
-    event PaymentReceived(string indexed who, uint256 value, bytes32 trahsactionId);    // Another blockchain
+    event PaymentReceived(address indexed who, uint256 value, uint256 total);                          // Native blockchain
+    event PaymentReceived(string indexed who, uint256 value, uint256 total, bytes32 trahsactionId);    // Another blockchain
 
     event PaymentWithdrawn(address indexed who, uint256 value);
     event PaymentWithdrawn(string indexed who, uint256 value, bytes32 transactionId);
 
-    event PaymentDone(address indexed who, uint256 value);
+    event PaymentDone(address indexed who, uint256 value, uint256 total);
+    event PaymentDone(string indexed who, uint256 value, uint256 total, bytes32 transactionId);
 
     constructor (address _deed) public {
         require(_deed != address(0));
@@ -56,13 +57,25 @@ contract Escrow is Owned, AddressChecker {
     }
 
     function _receive(address _who, uint256 _value) internal {
-        emit PaymentReceived(_who, _value);
-        _setPayment(_who, _value);
+        require(_value > 0, "Value must be greater zero!");
+        payment += _value;
+        if (checkPayment()) {
+            emit PaymentDone(_who, _value, payment);
+        }
+        else {
+            emit PaymentReceived(_who, _value, payment);
+        }
     }
 
     function _receive(string _who, uint256 _value, bytes32 _transactionId) internal {
-        emit PaymentReceived(_who, _value, _transactionId);
-        _setPayment(msg.sender, _value);
+        require(_value > 0, "Value must be greater zero!");
+        payment += _value;
+        if (checkPayment()) {
+            emit PaymentDone(_who, _value, payment, _transactionId);
+        }
+        else {
+            emit PaymentReceived(_who, _value, payment, _transactionId);
+        }
     }
 
     function _withdraw(address _who, uint256 _value) internal {
@@ -73,14 +86,6 @@ contract Escrow is Owned, AddressChecker {
     function _withdraw(string _who, uint256 _value, bytes32 _transactionId) internal {
         require(checkPayment(), "Payment is not done!");
         emit PaymentWithdrawn(_who, _value, _transactionId);
-    }
-
-    function _setPayment(address _sender, uint256 _value) private {
-        require(_value > 0, "Value must be greater zero!");
-        payment = _value;
-        if(checkPayment()) {
-            emit PaymentDone(_sender, payment);
-        }
     }
 
     function getType() public pure returns(uint8) {
