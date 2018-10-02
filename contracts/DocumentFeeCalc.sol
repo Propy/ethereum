8ea7c3cdc4e221751152878d493393f13db13031
Maxim Fischuk <mfischuk@vareger.com>
2018-10-02 15:09:57 +0300

    Added FeeCalc contract for Document Registry

---
+++ contracts/DocumentFeeCalc.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.4.24;
+
+import "./helpers/SafeMath.sol";
+import "./base/Owned.sol";
+
+
+contract FeeCalc is Owned {
+
+    using SafeMath for uint256;
+
+    uint256 baseFee;
+
+    constructor(uint256 _baseFee) public {
+        baseFee = _baseFee;
+    }
+
+    function getFee() public constant returns(uint256) {
+        return baseFee;
+    }
+
+    function getCompanyFee() public view returns (uint256) {
+        return getFee().div(3);
+    }
+
+    function getNetworkGrowthFee() public view returns (uint256) {
+        return getFee().sub(getCompanyFee());
+    }
+
+    function setFee(uint256 _baseFee) public onlyContractOwner {
+        baseFee = _baseFee;
+    }
+
+}
